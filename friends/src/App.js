97a7981349f6f6e6a33bf461b8d230aa6926c28a
Alberto Galvan <astrometrics@Unimatrix-Zero.local>
2019-11-12 17:23:54 -0600

    simplified the project, reducing file structure

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import PrivateRoute from './components/PrivateRoute';
-import Header from './components/Header';
+
 import Login from './components/Login';
-import Home from './components/Home';
+
 import FriendsList from './components/FriendsList';
 import AddFriend from './components/AddFriend';
 
